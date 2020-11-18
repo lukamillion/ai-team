minor sign fix

data_loader.py
@@ -76,8 +76,8 @@ class DataLoader():
         """
         T = 1/ (fps if fps>0 else self.fps)
 
-        vx = -np.diff( pos[:,0], append=pos[-1,0] ) / T
-        vy = -np.diff( pos[:,1], append=pos[-1,1] ) / T
+        vx = np.diff( pos[:,0], append=pos[-1,0] ) / T
+        vy = np.diff( pos[:,1], append=pos[-1,1] ) / T
         
         return vx, vy
         